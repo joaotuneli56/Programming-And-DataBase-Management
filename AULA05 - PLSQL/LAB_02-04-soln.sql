bd9e79ef35d57fadaf377f410b5e7870180609c2
joaotuneli56 <joaopedromouratuneli@gmail.com>
2024-08-07 21:44:41 -0300

    Ajuste

--- AULA05 - PLSQL/LAB_02-04-soln.sql
+++
@@ -1,9 +0,0 @@
-DECLARE
-    today DATE;
-    tomorrow today%TYPE;
-BEGIN 
-    tomorrow := SYSDATE + 1;
-    DBMS_OUTPUT.PUT_LINE ('Hello word');
-    DBMS_OUTPUT.PUT_LINE ('TODAY IS:' || today);
-    DBMS_OUTPUT.PUT_LINE ('TOMORROW IS: ' || tomorrow);
-END;
